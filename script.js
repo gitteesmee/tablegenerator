28bb7454ca5550c02c98f84a25435e0de6155bc1
gitteesmee <65610185+gitteesmee@users.noreply.github.com>
2020-07-06 16:23:28 +0200

    V13
    Edits made after comments Raymond

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 		function calculation() { // functie start
-		var y; // variabele y
-		var number; // variabele number
-		var txt; // variabele txt
+			var y; // variabele y
+			var number; // variabele number
+			var txt; // variabele txt
 
-		y = document.getElementById("num").value; // y is wat er in het input veld staat
+			y = document.getElementById("num").value; // y is wat er in het input veld staat
 
-		if (isNaN(y)) { //als y is 'not a number/geen nummer' dan
-			number = 1; // is het de tafel van 1
-			txt = "Input is not valid, put in a number please."; // tekst wordt weergegeven
-		} else { // anders
-			number = y; // de tafel is gelijk aan y
-			txt = ""; // er wordt geen tekst weergegeven
+			if (isNaN(y)) { //als y is 'not a number/geen nummer' dan
+				number = 1; // is het de tafel van 1
+				txt = "Input is not valid, put in a number please."; // tekst wordt weergegeven
+			} else { // anders
+				number = y; // de tafel is gelijk aan y
+				txt = ""; // er wordt geen tekst weergegeven
+				}
+
+			document.getElementById("txt").innerHTML = txt; // het id txt wordt gekoppeld aan txt
+
+			var x = number; // x is gelijk aan number
+
+			var outcome = ""; // outcome wordt later ingevuld, nu gelijk aan niks
+			var i = 0; // variabele i is 0
+			while (i<101){ // loop van 0 tot 101, oftewel tot en met 100
+				outcome += i + " x " + x + " = " + i * x + "<br>" ; // outcome is enter, '1' x 'x' = 'i*x'
+				i++; // er wordt 1 bij i opgeteld 
 			}
 
-		document.getElementById("txt").innerHTML = txt; // het id txt wordt gekoppeld aan txt
-
-		var x = number; // x is gelijk aan number
-
-		var outcome = ""; // outcome wordt later ingevuld, nu gelijk aan niks
-		var i = 0; // variabele i is 0
-		while (i<101){ // loop van 0 tot 101, oftewel tot en met 100
-			outcome += i + " x " + x + " = " + i * x + "<br>" ; // outcome is enter, '1' x 'x' = 'i*x'
-			i++; // er wordt 1 bij i opgeteld 
-		}
-
-		document.getElementById("multiply").innerHTML = outcome; // het id mutiply wordt gekoppeld aan multiply
+			document.getElementById("multiply").innerHTML = outcome; // het id mutiply wordt gekoppeld aan multiply
 
 		}
